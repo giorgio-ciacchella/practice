e7744006b937acd8dc1fb3b28b65eddb4d270e5b
giorgio <giorgio.ciacchella@gmail.com>
2023-07-17 21:45:00 +0200

    new progress

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,6 @@ public class Main {
    * B = 5 7 11 10 5 8
    * <p>
    * BS = [(1,0)(1,4)(2,1)(3,2)(4,0)(4,4)(5,5)]
-   * Problematic = [(1,0)(1,4)(4,0)(4,4)] => B = [0, 4]
    * Disjoint = [(2,1)(3,2)(5,5)]
    * <p>
    * <p>
@@ -44,52 +43,37 @@ public class Main {
     // Write your code here
     int result = 0;
 
-    final Map<Integer, List<Pair<Integer>>> beautifulMap = new HashMap<>();
+    final Map<Pair<Integer>, List<Pair<Integer>>> beautifulMap = new HashMap<>();
 
+    List<Pair<Integer>> beautifulPairs = new ArrayList<>();
     for (int i = 0; i < B.size(); i++) {
-      int currentB = B.get(i);
-      populateBeautifulMap(A, currentB, i, beautifulMap);
-    }
-    int maxDisjointSize = calculateMaxDisjointSize(beautifulMap);
-
-
-    Set<Integer> possibleValues = new HashSet<>(beautifulMap.keySet());
-    for (int bIndex = 0 ; bIndex < B.size(); bIndex++) {
-      int finalBIndex = bIndex;
-      int currentB = B.get(bIndex);
-      removeValueFromMap(beautifulMap, finalBIndex, currentB);
-
-
-      for (int aValue : possibleValues) {
-        populateBeautifulMap(A, aValue, bIndex, beautifulMap);
-        int newDisjointSize = calculateMaxDisjointSize(beautifulMap);
-        if (newDisjointSize > maxDisjointSize) {
-          maxDisjointSize = newDisjointSize;
-        }
-        removeValueFromMap(beautifulMap, finalBIndex, aValue);
-      }
+      int bValue = B.get(i);
+      beautifulPairs.addAll(calculatePairs(A, new Pair<>(i, bValue), beautifulMap));
     }
 
+    
 
 
-    return maxDisjointSize;
+
+    return beautifulPairs.size();
   }
 
-  private static void removeValueFromMap(Map<Integer, List<Pair<Integer>>> beautifulMap, int index, int value) {
-    if (!beautifulMap.containsKey(value)) return;
+  private static List<Pair<Integer>> calculatePairs(List<Integer> a, Pair<Integer> bPair, Map<Pair<Integer>, List<Pair<Integer>>> beautifulMap) {
 
-
-    //TODO put a b null value if we remove all the entries
-
-    List<Pair<Integer>> filteredList = beautifulMap.get(value)
-            .stream()
-            .filter(integerPair -> integerPair.b != index)
-            .collect(Collectors.toList());
-    if (filteredList.isEmpty()) {
-      beautifulMap.put(value, )
-    } else {
-      beautifulMap.put(value, filteredList);
+    if (beautifulMap.containsKey(bPair)) {
+      return beautifulMap.get(bPair);
     }
+
+    List<Pair<Integer>> beautifulPairs = new ArrayList<>();
+
+    for (int i = 0; i < a.size(); i++) {
+      int aValue = a.get(i);
+      if (aValue == bPair.b) {
+        beautifulPairs.add(new Pair<>(i, bPair.a));
+      }
+    }
+    beautifulMap.put(bPair, beautifulPairs);
+    return beautifulPairs;
   }
 
   private static int calculateMaxDisjointSize(Map<Integer, List<Pair<Integer>>> beautifulMap) {
@@ -151,35 +135,6 @@ public class Main {
     }
   }
 
-  private static void populateBeautifulMap(List<Integer> A, int currentB, int bIndex, Map<Integer, List<Pair<Integer>>> beautifulMap) {
-      if (beautifulMap.containsKey(currentB)) {
-        List<Pair<Integer>> newBeatifulList = beautifulMap.get(currentB)
-                .stream()
-                .flatMap(pair -> Optional.ofNullable(pair.b)
-                        .map(integer -> Stream.of(pair, new Pair<>(pair.a, bIndex)))
-                        .orElse(Stream.of(new Pair<>(pair.a, bIndex))))
-                .toList();
-        beautifulMap.put(currentB, newBeatifulList);
-      } else {
-        if (!beautifulMap.isEmpty()) {
-          return;
-        }
-        for (int j = 0; j < A.size(); j++) {
-          final int currentA = A.get(j);
-          if (currentA == currentB) {
-            int finalJ = j;
-
-            Optional.ofNullable(beautifulMap.get(currentA))
-                            .ifPresentOrElse(pairs -> beautifulMap.put(currentA, new ArrayList<>(pairs) {{
-                              add(new Pair<>(finalJ, bIndex));
-                            }}), () -> beautifulMap.put(currentA, List.of(new Pair<>(finalJ, bIndex))));
-
-          } else {
-            beautifulMap.put(currentA, List.of(new Pair<>(j, null)));
-          }
-        }
-      }
-  }
 
 
   public static void main(String[] args) {
