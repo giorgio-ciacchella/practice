cc3353bedeae6923e936e52923971074169848bd
giorgio <giorgio.ciacchella@gmail.com>
2023-07-18 22:28:50 +0200

    progress

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class Main {
 
@@ -40,117 +33,43 @@ public class Main {
    *    11: [[3,2]],
    *    8: [[5,5]]
    * }
+   *
+       *      0 1 2  3 4 5
+   *    * A = 3 5 7 11 5 8
+   *    * B = 5 7 11 3 5 8
    */
 
   public static int beautifulPairs(List<Integer> A, List<Integer> B) {
     // Write your code here
-    int result = 0;
-
-    final Map<Pair<Integer>, List<Pair<Integer>>> beautifulMap = new HashMap<>();
-
-    List<Pair<Integer>> beautifulPairs = new ArrayList<>();
-    for (int i = 0; i < B.size(); i++) {
-      int bValue = B.get(i);
-      beautifulPairs.addAll(calculatePairs(A, new Pair<>(i, bValue), beautifulMap));
-    }
-    beautifulPairs = getOnlyDisjoinPairs(beautifulPairs);
-    int maxSize = beautifulPairs.size();
-
-    for (int aValue : A) {
-      for (int i = 0; i < B.size(); i++) {
-        beautifulPairs.addAll(calculatePairs(A, new Pair<>(i, aValue), beautifulMap));
-        for (int j = 0; j < B.size(); j++) {
-          if (i==j) continue;
-
-          final Integer bValue = B.get(j);
-          beautifulPairs.addAll(calculatePairs(A, new Pair<>(j, bValue), beautifulMap));
-
-        }
-        beautifulPairs = getOnlyDisjoinPairs(beautifulPairs);
-        if (beautifulPairs.size() > maxSize) {
-          maxSize = beautifulPairs.size();
-        }
-        beautifulPairs.clear();
-      }
-    }
 
 
 
-    return maxSize;
+
+    return calculatePairs(A,B, new HashMap<>());
   }
 
-  private static List<Pair<Integer>> getOnlyDisjoinPairs(final List<Pair<Integer>> beautifulPairs) {
-    final Set<Integer> aIndexes = new HashSet<>();
-    final Set<Integer> bIndexes = new HashSet<>();
+  private static int calculatePairs(List<Integer> a, List<Integer> b, Map<List<Integer>, Integer> beautifulMap) {
 
-    return beautifulPairs.stream()
-        .filter(integerPair -> {
-          if (aIndexes.contains(integerPair.a) || bIndexes.contains(integerPair.b)) {
-            return false;
-          }
-          aIndexes.add(integerPair.a);
-          bIndexes.add(integerPair.b);
-          return true;
-        })
-        .collect(Collectors.toList());
-  }
+//    if (beautifulMap.containsKey(b)) {
+//      return beautifulMap.get(b);
+//    }
+    Set<Integer> aIndexes = new HashSet<>();
+    Set<Integer> bIndexes = new HashSet<>();
 
-  private static List<Pair<Integer>> calculatePairs(List<Integer> a, Pair<Integer> bPair, Map<Pair<Integer>, List<Pair<Integer>>> beautifulMap) {
+    int size = 0;
 
-    if (beautifulMap.containsKey(bPair)) {
-      return beautifulMap.get(bPair);
-    }
-
-    List<Pair<Integer>> beautifulPairs = new ArrayList<>();
-
+    int bValue = b.get(b.size() - 1);
     for (int i = 0; i < a.size(); i++) {
       int aValue = a.get(i);
-      if (aValue == bPair.b) {
-        beautifulPairs.add(new Pair<>(i, bPair.a));
+      if (bValue == aValue) {
+        return 1;
       }
     }
-    beautifulMap.put(bPair, beautifulPairs);
-    return beautifulPairs;
+
+//    beautifulMap.put(b, size);
+    return size;
   }
 
-  static final class Pair<T> {
-    private final T a;
-    private final T b;
-
-    Pair(T a, T b) {
-      this.a = a;
-      this.b = b;
-    }
-
-    public T a() {
-      return a;
-    }
-
-    public T b() {
-      return b;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-      if (obj == this) return true;
-      if (obj == null || obj.getClass() != this.getClass()) return false;
-      var that = (Pair) obj;
-      return Objects.equals(this.a, that.a) &&
-              Objects.equals(this.b, that.b);
-    }
-
-    @Override
-    public int hashCode() {
-      return Objects.hash(a, b);
-    }
-
-    @Override
-    public String toString() {
-      return "Pair[" +
-              "a=" + a + ", " +
-              "b=" + b + ']';
-    }
-  }
 
 
 
